d09f77fbf7607296d0f49c2c9da6c9bb0091e1a1
Jenny Robbins <Jenny.Robbins@softwire.com>
2022-10-24 12:00:20 +0100

    Made pawns move 1 square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,17 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let current_square = board.findPiece(this);
+        let move_square = new Square(current_square.row, current_square.col)
+        if (this.player == 0){
+            move_square.row = current_square.row + 1;
+        }
+        else{
+            move_square.row = current_square.row - 1;
+        }
+
+        let out_array = new Array();
+        out_array.push(move_square);
+        return out_array;
     }
 }
